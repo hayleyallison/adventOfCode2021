8fb3b94a1a22938aee217402d86e2e330e406c60
Hayley Allison <haliley44@gmail.com>
2021-12-06 22:48:51 +0100

    got rid of the unnecessary second dimension on the arrays in day6_b

--- day6_b.py
+++ day6_b.py
@@ -4,30 +4,25 @@ import numpy as np
 filename = "inputData/input6.txt"
 file     = open(filename)
 
-fish  = np.zeros((9, 2))
-blank = np.zeros((9, 2))
-
-#create fish cohorts
-for i in range(0,9):
-	fish[i, 0] = i
-	blank[i,0] = i
+fish  = np.zeros((9,)) #you have to use double because of an overfill
+blank = np.zeros((9,))
 
 strfishline   = file.read()
 fish_ages_int = strfishline.split(',')
 
 #count the fish!
 for fishbit in fish_ages_int:
-	a = np.where(fish[:,0] == int(fishbit))
-	fish[a,1] += 1
+	a = int(fishbit)
+	fish[a] += 1
 
 for day in range(0,256):
 	fish_update = blank.copy()
 	for f in range(0,9):
 		if f == 0:
-			fish_update[6,1] += fish[0,1]
-			fish_update[8,1] += fish[0,1]
+			fish_update[6] += fish[0]
+			fish_update[8] += fish[0]
 		else:
-			fish_update[f-1, 1] += fish[f,1] 
+			fish_update[f-1] += fish[f] 
 	fish = fish_update.copy()
 
-print(sum(fish[:,1]))
+print(sum(fish))
